f28739d1cda2083bbbe96a4de2b755fe6524facb
HoangPham1208 <hoang.pham1208@hcmut.edu.vn>
2024-07-24 11:02:47 +0700

    feat(add): add needed function for vault

--- tests/setup.go
+++ tests/setup.go
@@ -10,9 +10,10 @@ import (
 	"github.com/stretchr/testify/require"
 	"go.uber.org/zap"
 
-	"github.com/babylonchain/staking-queue-client/client"
+	// "github.com/babylonchain/staking-queue-client/client"
 	"github.com/babylonchain/staking-queue-client/config"
-	"github.com/babylonchain/staking-queue-client/queuemngr"
+	"github.com/scalarorg/staking-queue-client/client"
+	"github.com/scalarorg/staking-queue-client/queuemngr"
 )
 
 type TestServer struct {
@@ -81,18 +82,25 @@ func purgeQueues(conn *amqp091.Connection, queues []string) error {
 }
 
 // Scalar
-func buildNScalarStakingEvents(stakerHash string, numOfEvent int) []*client.ScalarStakingEvent {
-	var scalarStakingEvents []*client.ScalarStakingEvent
+func buildNScalarVaultEvents(stakerHash string, numOfEvent int) []*client.ActiveVaultEvent {
+	var scalarStakingEvents []*client.ActiveVaultEvent
 	for i := 0; i < numOfEvent; i++ {
-		scalarStakingEvent := client.NewScalarStakingEvent(
+		scalarStakingEvent := client.NewActiveVaultEvent(
 			"0x1234567890abcdef"+fmt.Sprint(i),
 			stakerHash,
 			"0xabcdef1234567890"+fmt.Sprint(i),
 			1+uint64(i),
 			100+uint64(i),
 			time.Now().Unix(),
-			200+uint64(i),
-			1+uint64(i),
+			0,
+			// 8 bytes chainID
+			[]byte{0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08},
+			// 20 bytes chainIdUserAddress
+			[]byte{0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09, 0x10, 0x11, 0x12, 0x13, 0x14, 0x15, 0x16, 0x17, 0x18, 0x19, 0x20},
+			// 20 bytes chainIdSmartContractAddress
+			[]byte{0x21, 0x22, 0x23, 0x24, 0x25, 0x26, 0x27, 0x28, 0x29, 0x30, 0x31, 0x32, 0x33, 0x34, 0x35, 0x36, 0x37, 0x38, 0x39, 0x40},
+			// 32 bytes amountVault
+			[]byte{0x41, 0x42, 0x43, 0x44, 0x45, 0x46, 0x47, 0x48, 0x49, 0x50, 0x51, 0x52, 0x53, 0x54, 0x55, 0x56, 0x57, 0x58, 0x59, 0x60, 0x61, 0x62, 0x63, 0x64, 0x65, 0x66, 0x67, 0x68, 0x69, 0x70, 0x71, 0x72},
 			"0xabcdef1234567890"+fmt.Sprint(i),
 			false,
 		)
